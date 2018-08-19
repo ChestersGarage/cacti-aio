1e24f96f3fe67e28f38a2df949f0246f38a3b191
Mark Chester <mark@chesterfamily.org>
2018-08-18 17:05:44 -0700

    Operational with some tweaks v0.2

--- init-services.sh
+++ init-services.sh
@@ -1,10 +1,18 @@
 #!/bin/sh -x
 
+# This is as bare-bones as you can get.
+# To do: make this robust, maybe with a supervisor.
+
+# Start MySQL/MariaDB
 nohup /usr/bin/mysqld_safe --datadir="/var/lib/mysql" &
+# Since we break off, we need to give it time to start up.
 sleep 3
 
+# Start cron
 crond -L /var/log/cron
 
+# Start Apache
 /usr/sbin/httpd -k start -f /etc/apache2/httpd.conf
 
+# Hold open the session
 tail -f /var/log/cacti/cacti.log
